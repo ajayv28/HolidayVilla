023cfdc5dff4d97efb844bc5d57e9a2889a6dc25
ajayv28 <ajayvengateshbabu@gmail.com>
2024-09-07 15:20:40 +0300

    creation of git

---
+++ src/test/java/com/ajay/HolidayVilla/HolidayVillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.HolidayVilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayVillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
