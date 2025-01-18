0dda7acf311946a34fb00fcebcf03506a1025fee
ajayv28 <ajayvengateshbabu@gmail.com>
2025-01-18 12:23:16 +0300

    updated target folder files

---
+++ target/classes/static/js/staff_script.js
@@ -0,0 +1,158 @@
+import { camelCaseToNormal, jsonToText, postingFunction, extractKeys, getValue, getOrPutFunction} from './common_script.js';
+
+const links = document.querySelectorAll(".header a");
+
+links.forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add("active"); 
+    }
+}); //to show active in header
+
+
+document.getElementById("logoutButton").addEventListener("click", async function() {
+    await fetch("http://localhost:8081/logout", {
+        method: "POST",
+        credentials: "include"
+    });
+    window.location.href = "/home.html";  
+});
+
+// JS CODE FOR STAFF.HTML    *****************************************************************
+
+document.getElementById("staff-posting-closePopup").addEventListener("click", function() {
+    document.getElementById("staff-posting-popup").style.display = "none";
+});
+
+document.getElementById("staff-getter-closePopup").addEventListener("click", function() {
+    document.getElementById("staff-getter-popup").style.display = "none";
+});
+
+
+document.getElementById("staffRegisterForm").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const heading = document.getElementById("staff-posting-responseHeading");
+    const message = document.getElementById("staff-posting-responseMessage");
+    const popup = document.getElementById("staff-posting-popup");
+    const api = "http://localhost:8081/api/staff/onBoard";
+    postingFunction(this, api, "Below is the onboarded staff detail", "Staff is on-boarded successfully", heading, message, popup);
+});
+
+document.getElementById("formOffBoardStaff").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const mail = document.getElementById("offBoardStaff").value;
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = `http://localhost:8081/api/staff/offBoard?staffEmail=${mail}`;
+    getOrPutFunction("PUT", api, table, popup, heading, "Staff Off-Boarded Successfully");
+});
+
+document.getElementById("formGetStaffByStaffEmail").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const mail = document.getElementById("getStaffByStaffEmail").value;
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = `http://localhost:8081/api/staff/get-staff-by-staffEmail?staffEmail=${mail}`;
+    getOrPutFunction("GET", api, table, popup, heading, "Staff Detail:");
+});
+
+document.getElementById("formResetPassword").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const mail = document.getElementById("resetPasswordEmail").value;
+    const pass = document.getElementById("resetPasswordPassword").value;
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = `http://localhost:8081/api/staff/reset-password?staffEmail=${mail}&newPassword=${pass}`;
+    getOrPutFunction("PUT", api, table, popup, heading, "Password Reset Successful:");
+});
+
+document.getElementById("formChangeDepartment").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const mail = document.getElementById("changeDepartmentStaffEmail").value;
+    const dept = document.getElementById("changeDepartmentDepartment").value;
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = `http://localhost:8081/api/staff/change-department?staffEmail=${mail}&department=${dept}`;
+    getOrPutFunction("PUT", api, table, popup, heading, "Department Change Successful:");
+});
+
+document.getElementById("getAllCurrentStaff").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = "http://localhost:8081/api/staff/all-current-staff";
+    getOrPutFunction("GET", api, table, popup, heading, "All Current Staff");
+});
+
+
+document.getElementById("getAllExStaff").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = "http://localhost:8081/api/staff/all-ex-staff";
+    getOrPutFunction("GET", api, table, popup, heading, "All Ex Staff");
+});
+
+document.getElementById("formGetAllCurrentStaffByDepartment").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const dept = document.getElementById("getAllCurrentStaffByDepartment").value;
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = `http://localhost:8081/api/staff/all-current-staff-by-department?department=${dept}`;
+    getOrPutFunction("GET", api, table, popup, heading, `All Current Staff of ${dept}` );
+});
+
+document.getElementById("formGetAllExStaffByDepartment").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const dept = document.getElementById("getAllExStaffByDepartment").value;
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = `http://localhost:8081/api/staff/all-ex-staff-by-department?department=${dept}`;
+    getOrPutFunction("GET", api, table, popup, heading, `All Ex Staff of ${dept}` );
+});
+
+document.getElementById("formGetStaffSalaryByStaffEmail").addEventListener("submit", async function(event){ 
+    event.preventDefault();
+    const mail = document.getElementById("getStaffSalaryByStaffEmail").value;
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = `http://localhost:8081/api/staff/get-staff-salary-by-staffEmail?staffEmail=${mail}`;
+    const response = await fetch(api, {
+        method: "GET", 
+        });
+    heading.innerText = `Salary of given staff: ${response}`;
+    popup.style.display = "block";
+});
+
+document.getElementById("formChangeStaffSalaryByStaffEmail").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const mail = document.getElementById("changeStaffSalaryByStaffEmailStaffEmail").value;
+    const salary = document.getElementById("changeStaffSalaryByStaffEmailNewSalary").value;
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = `http://localhost:8081/api/staff/change-staff-salary-by-staffEmail?staffEmail=${mail}&newSalary=${salary}`;
+    getOrPutFunction("PUT", api, table, popup, heading, "Salary of given staff updated successfully" );
+});
+
+document.getElementById("createTransactionForPayroll").addEventListener("submit", function(event){ 
+    event.preventDefault();
+    const heading = document.getElementById("staff-getter-responseHeading");
+    const table = document.getElementById("staff-getter-responseTable");
+    const popup = document.getElementById("staff-getter-popup");
+    const api = "http://localhost:8081/api/staff/create-transaction-for-payroll";
+    getOrPutFunction("PUT", api, table, popup, heading, "Salary Transactions has been created for all current staff successfully" );
+});
+
+
+
+
+
+
